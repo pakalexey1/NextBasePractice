6af1d2a774edb3eb041ace5af350b64048efca21
pakalexey <mye604@gmail.com>
2022-07-22 17:53:35 -0400

    Reconnect to Sasha's zoom using a utility written with a Robot object! Wow so cool!

--- src/test/java/com/cydeo/step_definitions/Hook.java
+++ src/test/java/com/cydeo/step_definitions/Hook.java
@@ -3,12 +3,13 @@ package com.cydeo.step_definitions;
 import com.cydeo.utilties.Driver;
 import io.cucumber.java.Scenario;
 import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Hook {
 
-    @After
+        @After
     public void tearDown(Scenario scenario) {
 
         if (scenario.isFailed()) {
